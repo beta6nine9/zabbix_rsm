051c606cd9519f16a7690d90db5676760667bfae
Andrejs Sitals <andrejs.sitals@zabbix.com>
2019-10-15 12:08:23 +0300

    ....I..... [RSM-1] 538: sla-api-recent.pl: don't re-process last processed cycle

--- opt/zabbix/scripts/sla-api-recent.pl
+++ opt/zabbix/scripts/sla-api-recent.pl
@@ -572,11 +572,11 @@ sub add_cycles($$$$$$$$$$$)
 
 	return if ($lastclock == $lastclock_db);	# we are up-to-date, according to cache
 
-	my $cycle_start = cycle_start($lastclock, $delay);
+	my $cycle_start = cycle_start($lastclock + $delay, $delay);
 
 	my $db_cycle_start = cycle_start($lastclock_db, $delay);
 
-	my $max_clock = cycle_end($cycle_start + $max_period, $delay);
+	my $max_clock = cycle_end($cycle_start - $delay + $max_period, $delay);
 
 	# issue #511
 	# Sometimes we get strange timestamp of cycle to calculate, e. g. 960 .
@@ -662,7 +662,7 @@ sub cycles_to_calculate($$$$$$$$)
 
 			if (opt('now'))
 			{
-				$lastclock = cycle_start(getopt('now'), $delay);
+				$lastclock = cycle_start(getopt('now') - $delay, $delay);
 
 				dbg("using specified last clock: ", ts_str($lastclock));
 			}
