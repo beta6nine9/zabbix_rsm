4aa242a5e18b50bb6d17b5b1b2fa343c89931ce7
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2019-11-11 19:21:47 +0200

    .......... [RSM-1] 595: replacing comment with an enum

--- src/libs/zbxdbupgrade/dbupgrade_4040.c
+++ src/libs/zbxdbupgrade/dbupgrade_4040.c
@@ -44,12 +44,12 @@ static int	DBpatch_4040300(void)
 
 static int	DBpatch_4040301(void)
 {
-	/* type=3 -> type=simple check */
 	if (ZBX_DB_OK > DBexecute(
 		"update items"
 		" set delay='{$RSM.DNS.UDP.DELAY}'"
 		" where key_ like 'rsm.dns.udp[%%'"
-		" and type=3"))
+		" and type=%d",
+		ITEM_TYPE_SIMPLE))
 	{
 		return FAIL;
 	}
@@ -59,12 +59,12 @@ static int	DBpatch_4040301(void)
 
 static int	DBpatch_4040302(void)
 {
-	/* type=3 -> type=simple check */
 	if (ZBX_DB_OK > DBexecute(
 		"update items"
 		" set delay='{$RSM.DNS.TCP.DELAY}'"
 		" where key_ like 'rsm.dns.tcp[%%'"
-		" and type=3"))
+		" and type=%d",
+		ITEM_TYPE_SIMPLE))
 	{
 		return FAIL;
 	}
@@ -74,12 +74,12 @@ static int	DBpatch_4040302(void)
 
 static int	DBpatch_4040303(void)
 {
-	/* type=3 -> type=simple check */
 	if (ZBX_DB_OK > DBexecute(
 		"update items"
 		" set delay='{$RSM.RDDS.DELAY}'"
 		" where key_ like 'rsm.rdds[%%'"
-		" and type=3"))
+		" and type=%d",
+		ITEM_TYPE_SIMPLE))
 	{
 		return FAIL;
 	}
@@ -89,29 +89,31 @@ static int	DBpatch_4040303(void)
 
 static int	DBpatch_4040304(void)
 {
-	/* type=3 -> type=simple check */
 	if (ZBX_DB_OK > DBexecute(
 		"update items"
 		" set delay='{$RSM.RDAP.DELAY}'"
 		" where key_ like 'rdap[%%'"
-		" and type=3"))
+		" and type=%d",
+		ITEM_TYPE_SIMPLE))
 	{
 		return FAIL;
 	}
+
 	return SUCCEED;
 }
 
 static int	DBpatch_4040305(void)
 {
-	/* type=3 -> type=simple check */
 	if (ZBX_DB_OK > DBexecute(
 		"update items"
 		" set delay='{$RSM.EPP.DELAY}'"
 		" where key_ like 'rsm.epp[%%'"
-		" and type=3"))
+		" and type=%d",
+		ITEM_TYPE_SIMPLE))
 	{
 		return FAIL;
 	}
+
 	return SUCCEED;
 }
 
