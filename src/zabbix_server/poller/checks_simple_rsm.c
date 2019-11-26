2326e25631c7c759881781845c9f913eff8e99c3
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2019-11-26 16:54:10 +0200

    .......... [RSM-1] 613: fixing zbx_sprintf

--- src/zabbix_server/poller/checks_simple_rsm.c
+++ src/zabbix_server/poller/checks_simple_rsm.c
@@ -1252,7 +1252,7 @@ static int	zbx_dns_in_a_query(ldns_pkt **pkt, ldns_resolver *res, const ldns_rdf
 
 	if (LDNS_STATUS_OK != status)
 	{
-		zbx_snprintf(err, err_size, "Could not create query packet");
+		zbx_snprintf(err, err_size, "Could not create query packet: %s", ldns_get_errorstr_by_id(status));
 		goto err;
 	}
 
@@ -1271,8 +1271,12 @@ static int	zbx_dns_in_a_query(ldns_pkt **pkt, ldns_resolver *res, const ldns_rdf
 
 	if (LDNS_STATUS_OK != status)
 	{
-		zbx_snprintf(err, err_size, "Could not send query");
+		zbx_snprintf(err, err_size, "Could not send query: %s", ldns_get_errorstr_by_id(status));
 		goto err;
+	}
+	else
+	{
+		ret = SUCCEED;
 	}
 
 	recv_nsid = ldns_pkt_edns_data(*pkt);
@@ -1280,11 +1284,7 @@ static int	zbx_dns_in_a_query(ldns_pkt **pkt, ldns_resolver *res, const ldns_rdf
 	if (NULL == recv_nsid)
 	{
 		rsm_infof(NULL, " Could not get EDNS data\n");
-		if (LDNS_STATUS_OK == status)
-		{
-			ret = SUCCEED;
-			goto out;
-		}
+		goto out;
 	}
 
 	uint8_t	*data = ldns_rdf_data(recv_nsid);
@@ -1300,7 +1300,7 @@ static int	zbx_dns_in_a_query(ldns_pkt **pkt, ldns_resolver *res, const ldns_rdf
 
 		if (LDNS_EDNS_NSID == opt_code)
 		{
-			rsm_infof(NULL, ", returned NSID: \"%.*s\"\n", (int)opt_len, data);
+			rsm_infof(NULL, "Returned NSID: \"%.*s\"\n", (int)opt_len, data);
 			break;
 		}
 
@@ -1309,18 +1309,12 @@ static int	zbx_dns_in_a_query(ldns_pkt **pkt, ldns_resolver *res, const ldns_rdf
 	}
 
 	if (0 == size)
-		zbx_snprintf(err, err_size, "No EDNS NSID option was returned");
+		rsm_infof(NULL, "No EDNS NSID option was returned \n");
 
-	if (LDNS_STATUS_OK == status)
-	{
-		ret = SUCCEED;
-		goto out;
-	}
+	goto out;
 
 err:
 	sec = zbx_time() - sec;
-
-	zbx_snprintf(err, err_size, "cannot connect to nameserver: %s", ldns_get_errorstr_by_id(status));
 
 	switch (status)
 	{
