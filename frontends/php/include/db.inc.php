d9e9baec122ae497f473d49af636f74ba59d71ac
Gregory Chalenko <gregory.chalenko@zabbix.com>
2019-11-28 14:03:35 +0200

    ..F....... [RSM-1] fix for mysql sceure connection, applied changes from d6ac27fcaf8 for db.inc.php

--- frontends/php/include/db.inc.php
+++ frontends/php/include/db.inc.php
@@ -50,8 +50,23 @@ function DBconnect(&$error) {
 	else {
 		switch ($DB['TYPE']) {
 			case ZBX_DB_MYSQL:
-				$DB['DB'] = @mysqli_connect($DB['SERVER'], $DB['USER'], $DB['PASSWORD'], $DB['DATABASE'], $DB['PORT']);
-				if (!$DB['DB']) {
+				$ssl_connection = false;
+
+				$DB['DB'] = mysqli_init();
+
+				if ($DB['DB_CONN_TIMEOUT'] !== null) {
+					mysqli_options($DB['DB'], MYSQLI_OPT_CONNECT_TIMEOUT, $DB['DB_CONN_TIMEOUT']);
+				}
+
+				if ($DB['DB_KEY_FILE'] !== null || $DB['DB_CERT_FILE'] !== null || $DB['DB_CA_FILE'] !== null
+						|| $DB['DB_CA_PATH'] !== null || $DB['DB_CA_CIPHER'] !== null) {
+					mysqli_ssl_set($DB['DB'], $DB['DB_KEY_FILE'], $DB['DB_CERT_FILE'], $DB['DB_CA_FILE'],
+						$DB['DB_CA_PATH'], $DB['DB_CA_CIPHER']);
+					$ssl_connection = true;
+				}
+
+				if (!@mysqli_real_connect($DB['DB'], $DB['SERVER'], $DB['USER'], $DB['PASSWORD'], $DB['DATABASE'],
+						$DB['PORT'])) {
 					$error = 'Error connecting to database: '.trim(mysqli_connect_error());
 					$result = false;
 				}
@@ -61,6 +76,15 @@ function DBconnect(&$error) {
 				}
 				else {
 					DBexecute('SET NAMES utf8');
+				}
+
+				if ($result && $ssl_connection) {
+					$ssl = DBfetch(DBselect("SHOW STATUS LIKE 'Ssl_cipher'"));
+
+					if (!$ssl || $ssl['Value'] === '') {
+						$error = 'Established connection is not secure';
+						$result = false;
+					}
 				}
 
 				if ($result) {
