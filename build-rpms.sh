c51bc5205b9b93341114b7b1f3392324d8534183
Andrejs Sitals <andrejs.sitals@zabbix.com>
2020-04-02 13:26:29 +0300

    .......... [RSM-1] bumped version to rsm1.3.0fix672

--- build-rpms.sh
+++ build-rpms.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RSM_VERSION="rsm1.3.0"	# MAJOR.PROD.QA
+RSM_VERSION="rsm1.3.0fix672"	# MAJOR.PROD.QA
 RPMDIR="rpmbuild"
 SRV_VERSION_FILE="include/version.h"
 FE_VERSION_FILE="frontends/php/include/defines.inc.php"
