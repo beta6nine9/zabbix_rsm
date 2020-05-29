3f8b00dceb606d5ac494dd65d017b99f6ec5af9b
Gregory Chalenko <gregory.chalenko@zabbix.com>
2020-05-29 17:05:09 +0300

    ..F....... [RSM-1] simplified permission check; fixed media tab being shown in user profile form

--- ui/modules/RSM/security/Permission.php
+++ ui/modules/RSM/security/Permission.php
@@ -42,38 +42,22 @@ class Permission {
 		switch (CWebUser::getType()) {
 			case USER_TYPE_ZABBIX_USER:
 			case USER_TYPE_ZABBIX_ADMIN:
-				return !in_array($route, self::MODULE_ROUTES);
-
 			case USER_TYPE_SUPER_ADMIN:
 				return true;
 
 			case USER_TYPE_READ_ONLY:
-				return in_array($route, ['rsm.rollingweekstatus', 'rsm.incidents', 'rsm.slareports']);
-
-			case USER_TYPE_POWER_USER:
-				return in_array($route, [
-					// Monitoring.
-					'dashboard.view', 'overview.php', 'web.view', 'latest.php', 'tr_status.php', 'events.php',
-					'charts.php', 'screens.php', 'map.view', 'srv_status.php',
-					// Registrar/Registry monitoring.
-					'rsm.rollingweekstatus', 'rsm.incidents', 'rsm.slareports',
-					// Inventory.
-					'hostinventoriesoverview.php', 'hostinventories.php',
-					// Reports.
-					'report2.php', 'toptriggers.php',
+				return in_array($route, self::MODULE_ROUTES) || in_array($route, [
+					'history.php', 'userprofile.edit'
 				]);
 
 			case USER_TYPE_COMPLIANCE:
-				return in_array($route, [
-					// Monitoring
-					'dashboard.view', 'overview.php', 'web.view', 'latest.php', 'tr_status.php', 'events.php',
-					'charts.view', 'charts.view.json', 'screens.php', 'map.view', 'srv_status.php',
-					// Registrar/Registry monitoring.
-					'rsm.rollingweekstatus', 'rsm.incidents', 'rsm.slareports',
-					// Inventory
-					'hostinventoriesoverview.php', 'hostinventories.php',
-					// Reports
-					'report2.php', 'toptriggers.php'
+			case USER_TYPE_POWER_USER:
+				return in_array($route, self::MODULE_ROUTES) || !in_array($route, [
+					'actionconf.php', 'applications.php', 'conf.import.php',
+					'disc_prototypes.php', 'discoveryconf.php', 'graphs.php', 'host_discovery.php', 'host_prototypes.php',
+					'hostgroups.php', 'hosts.php', 'httpconf.php', 'items.php', 'maintenance.php', 'report4.php',
+					'services.php', 'templates.php', 'trigger_prototypes.php', 'triggers.php',
+					'auditacts.php', 'correlation.php', 'queue.php',
 				]);
 		}
 
