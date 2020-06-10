e6e697b4e957fec2e9e5246388171a597712d294
dimir <vladimir.levijev@zabbix.com>
2020-06-10 20:00:15 +0300

    ....I..... [RSM-1] test-results.pl: apply latest changes to the test script

--- opt/zabbix/scripts/tests/test-results.pl
+++ opt/zabbix/scripts/tests/test-results.pl
@@ -22,7 +22,7 @@ $Data::Dumper::Indent = 1;      # 1 provides less indentation instead of 2
 parse_opts();
 setopt('nolog');
 
-use constant DIR => path($0)->parent()->realpath()->stringify() . '/probe-results-data';
+use constant DIR => path($0)->parent()->realpath()->stringify() . '/test-results-data';
 
 sub read_service_data($$$$$$$$)
 {
@@ -91,7 +91,6 @@ my ($itemids_uint, $itemids_float, $itemids_str, $results_uint, $results_float, 
 
 my $services = {
 	'dns' => 60,
-	'dnssec' => 60,
 	'rdds' => 300,
 	'rdap' => 300,
 };
@@ -99,6 +98,8 @@ my $services = {
 my $now = time() - 3600 * 24 * 1;
 
 set_slv_config(get_rsm_config());
+
+db_connect();
 
 foreach my $service (sort(keys(%{$services})))
 {
@@ -116,64 +117,42 @@ foreach my $service (sort(keys(%{$services})))
 		\$item_data
 	);
 
-#	print("$service itemids_uint:\n", Dumper($itemids_uint));
-#	print("$service results_uint:\n", Dumper($results_uint));
-#	print("$service itemids_float:\n", Dumper($itemids_float));
-#	print("$service results_float:\n", Dumper($results_float));
-#	print("$service itemids_str:\n", Dumper($itemids_str));
-#	print("$service results_str:\n", Dumper($results_str));
-#	print("$service item_data:\n", Dumper($item_data));
-
-#	db_connect();
-#	setopt('debug');
-#	my $probe_history = get_test_history(
-#		$from,
-#		$till,
-#		$itemids_uint,
-#		$itemids_float,
-#		$itemids_str,
-#		\$results_uint,
-#		\$results_float,
-#		\$results_str
-#	);
-#	db_disconnect();
-#
-#	print("$service probe history:\n", Dumper($probe_history));
-#
-#	exit;
-
-	my $probe_results = get_test_results(
-		$service,
+	my $results = get_test_results(
 		[@{$results_uint}, @{$results_float}, @{$results_str}],
-		$item_data
+		$item_data,
+		$service
 	);
 
 	my ($buf, $error);
 
-	my $file = "@{[DIR]}/$service-expected-probe-results.json";
+	my $file = "@{[DIR]}/$service-expected-test-results.json";
 
 	if (SUCCESS != read_file($file, \$buf, \$error))
 	{
 		fail("$file: $error");
 	}
 
-	my $expected_probe_results = decode_json($buf);
+	my $expected_results = decode_json($buf);
 
 	my $c = new Data::Compare;
-	my $res = $c->Cmp($probe_results, $expected_probe_results);
+	my $res = $c->Cmp($results, $expected_results);
 
 	print("$service\t: collected and expected probe results are ", ($res ? "" : "not "), "identical.\n");
 
 	if (!$res)
 	{
 		print("---------------------------------------------\n");
-		print("Collected results:\n", Dumper($probe_results));
+		print("Collected results:\n", Dumper($results));
 		print("---------------------------------------------\n");
-		print("Expected results:\n", Dumper($expected_probe_results));
+		print("Expected results:\n", Dumper($expected_results));
 		print("Error: collected $service data structure is unexpected, see above\n");
+
+		db_disconnect();
 
 		exit -1;
 	}
 }
 
-print("OK: function RSMSLV::probe_results() is working correctly.\n");
+db_disconnect();
+
+print("OK: function RSMSLV::results() is working correctly.\n");
