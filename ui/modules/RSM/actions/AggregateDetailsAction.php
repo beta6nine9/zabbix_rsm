849538ef05b997042a3183121a9a3c62057d20b5
dimir <vladimir.levijev@zabbix.com>
2020-06-13 15:21:57 +0300

    ..F....... [RSM-1] do not set invalid defaults for RTT.HIGH values and do not keep them in the code at all

--- ui/modules/RSM/actions/AggregateDetailsAction.php
+++ ui/modules/RSM/actions/AggregateDetailsAction.php
@@ -331,8 +331,8 @@ class AggregateDetailsAction extends Action {
 		$defaults = [
 			CALCULATED_ITEM_DNS_DELAY => null,
 			CALCULATED_ITEM_DNS_AVAIL_MINNS => null,	// TODO: remove 3 months after deployment
-			CALCULATED_ITEM_DNS_TCP_RTT_HIGH => 500,
-			CALCULATED_ITEM_DNS_TCP_RTT_HIGH => 1500
+			CALCULATED_ITEM_DNS_TCP_RTT_HIGH => null,
+			CALCULATED_ITEM_DNS_TCP_RTT_HIGH => null
 		];
 		$macro = $this->getMacroHistoryValue(array_keys($defaults), $time_from);
 
