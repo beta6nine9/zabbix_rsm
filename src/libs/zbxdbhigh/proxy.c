dd467a537a1e08ccb74aad59bd5d45b1913de97b
Andrejs Sitals <andrejs.sitals@zabbix.com>
2020-12-07 20:28:22 +0200

    .......PS. [RSM-1] fixed compiler warning

--- src/libs/zbxdbhigh/proxy.c
+++ src/libs/zbxdbhigh/proxy.c
@@ -3517,7 +3517,7 @@ static int	process_history_data_by_itemids(zbx_socket_t *sock, zbx_client_item_v
 					0 == strncmp(items[i].key_orig, "resolver", ZBX_CONST_STRLEN("resolver"))))
 			{
 				zabbix_log(LOG_LEVEL_WARNING,
-						"skipping [" ZBX_FS_UI64 "; %s; %s] value [%s] bacause it is %d seconds old",
+						"skipping [" ZBX_FS_UI64 "; %s; %s] value [%s] bacause it is " ZBX_FS_I64 " seconds old",
 						items[i].itemid, items[i].host.host, items[i].key_orig,
 						values[i].value, now - values[i].ts.sec);
 
