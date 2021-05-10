cee408194e4b6c56fe9fabbe7a150ca8b52107bb
Andrejs Sitals <andrejs.sitals@zabbix.com>
2021-05-10 23:31:09 +0300

    ....I..... [RSM-1] 720: split '{$RSM.TLD.RDDS.ENABLED}' into RDDS43 and RDDS80 (DB upgrade patches)

--- src/libs/zbxdbupgrade/dbupgrade_5000.c
+++ src/libs/zbxdbupgrade/dbupgrade_5000.c
@@ -60,6 +60,49 @@ do															\
 }															\
 while (0)
 
+/* selects single value of zbx_uint64_t type from the database */
+#define SELECT_VALUE_UINT64(target_variable, query, ...)								\
+															\
+do															\
+{															\
+	DB_RESULT	__result;											\
+	DB_ROW		__row;												\
+															\
+	__result = DBselect(query, __VA_ARGS__);									\
+															\
+	/* check for errors */												\
+	if (NULL == __result)												\
+	{														\
+		zabbix_log(LOG_LEVEL_CRIT, "%s() on line %d: query failed", __func__, __LINE__);			\
+		goto out;												\
+	}														\
+															\
+	__row = DBfetch(__result);											\
+															\
+	/* check if there's at least one row in the resultset */							\
+	if (NULL == __row)												\
+	{														\
+		zabbix_log(LOG_LEVEL_CRIT, "%s() on line %d: query did not return any rows", __func__, __LINE__);	\
+		DBfree_result(__result);										\
+		goto out;												\
+	}														\
+															\
+	ZBX_STR2UINT64(target_variable, __row[0]);									\
+															\
+	__row = DBfetch(__result);											\
+															\
+	/* check that there are no more rows in the resultset */							\
+	if (NULL != __row)												\
+	{														\
+		zabbix_log(LOG_LEVEL_CRIT, "%s() on line %d: query returned more than one row", __func__, __LINE__);	\
+		DBfree_result(__result);										\
+		goto out;												\
+	}														\
+															\
+	DBfree_result(__result);											\
+}															\
+while (0)
+
 extern unsigned char	program_type;
 
 /*
@@ -219,6 +262,220 @@ out:
 	return ret;
 }
 
+/* 5000002, 5 - split {$RSM.TLD.RDDS.ENABLED} macro into {$RSM.TLD.RDDS43.ENABLED} and {$RSM.TLD.RDDS80.ENABLED} */
+static int	DBpatch_5000002_5(void)
+{
+	int		ret = FAIL;
+
+	DB_RESULT	result = NULL;
+	DB_ROW		row;
+
+	ONLY_SERVER();
+
+	result = DBselect("select hostmacroid,hostid,value,type from hostmacro where macro='{$RSM.TLD.RDDS.ENABLED}'");
+
+	if (NULL == result)
+		goto out;
+
+#define SQL_UPDATE	"update hostmacro set macro='%s',description='%s' where hostmacroid=%s"
+#define SQL_INSERT	"insert into hostmacro set hostmacroid=" ZBX_FS_UI64 ",hostid=%s,macro='%s',value='%s',description='%s',type='%s'"
+
+#define RDDS43_MACRO	"{$RSM.TLD.RDDS43.ENABLED}"
+#define RDDS80_MACRO	"{$RSM.TLD.RDDS80.ENABLED}"
+
+#define RDDS43_DESCR	"Indicates whether RDDS43 is enabled on the rsmhost"
+#define RDDS80_DESCR	"Indicates whether RDDS80 is enabled on the rsmhost"
+
+	while (NULL != (row = DBfetch(result)))
+	{
+		const char	*hostmacroid = row[0];
+		const char	*hostid      = row[1];
+		const char	*value       = row[2];
+		const char	*type        = row[3];
+
+		DB_EXEC(SQL_UPDATE, RDDS43_MACRO, RDDS43_DESCR, hostmacroid);
+		DB_EXEC(SQL_INSERT, DBget_maxid_num("hostmacro", 1), hostid, RDDS80_MACRO, value, RDDS80_DESCR, type);
+	}
+
+#undef RDDS43_MACRO
+#undef RDDS80_MACRO
+
+#undef RDDS43_DESCR
+#undef RDDS80_DESCR
+
+#undef SQL_UPDATE
+#undef SQL_INSERT
+
+	ret = SUCCEED;
+out:
+	DBfree_result(result);
+
+	return ret;
+}
+
+/* 5000002, 6 - replace {$RSM.TLD.RDDS.ENABLED} macro with {$RSM.TLD.RDDS43.ENABLED} and {$RSM.TLD.RDDS80.ENABLED} in rsm.dns[] and rsm.rdds[] item keys */
+static int	DBpatch_5000002_6(void)
+{
+	int	ret = FAIL;
+
+	ONLY_SERVER();
+
+#define FROM	",{$RSM.TLD.RDDS.ENABLED},"
+#define TO	",{$RSM.TLD.RDDS43.ENABLED},{$RSM.TLD.RDDS80.ENABLED},"
+
+	DB_EXEC("update items set key_=replace(key_,'%s','%s') where key_ like 'rsm.dns[%%]'", FROM, TO);
+	DB_EXEC("update items set key_=replace(key_,'%s','%s') where key_ like 'rsm.rdds[%%]'", FROM, TO);
+
+#undef FROM
+#undef TO
+
+	ret = SUCCEED;
+out:
+	return ret;
+}
+
+/* 5000002, 7 - split rdds.enabled item into rdds43.enabled and rdds80.enabled */
+static int	DBpatch_5000002_7(void)
+{
+	int		ret = FAIL;
+
+	DB_RESULT	result = NULL;
+	DB_ROW		row;
+
+	zbx_uint64_t	rdds43_templateid;
+	zbx_uint64_t	rdds80_templateid;
+
+	ONLY_SERVER();
+
+	/* update rdds43.enabled template item */
+
+	SELECT_VALUE_UINT64(rdds43_templateid, "select itemid from items where templateid is null and key_='%s'", "rdds.enabled");
+
+#define SQL	"update"												\
+			" items"											\
+		" set"													\
+			" name='RDDS43 enabled/disabled',"								\
+			"key_='rdds43.enabled',"									\
+			"params='{$RSM.TLD.RDDS43.ENABLED}',"								\
+			"description='History of RDDS43 being enabled or disabled.'"					\
+		" where"												\
+			" itemid=" ZBX_FS_UI64
+
+	DB_EXEC(SQL, rdds43_templateid);
+
+#undef SQL
+
+	/* create rdds80.enabled template item */
+
+	rdds80_templateid = DBget_maxid_num("items", 1);
+
+#define SQL	"insert into items ("											\
+			"itemid,type,snmp_oid,hostid,name,key_,delay,history,trends,status,value_type,"			\
+			"trapper_hosts,units,formula,logtimefmt,templateid,valuemapid,params,ipmi_sensor,authtype,"	\
+			"username,password,publickey,privatekey,flags,interfaceid,description,inventory_link,lifetime,"	\
+			"evaltype,jmx_endpoint,master_itemid,timeout,url,query_fields,posts,status_codes,"		\
+			"follow_redirects,post_type,http_proxy,headers,retrieve_mode,request_method,output_format,"	\
+			"ssl_cert_file,ssl_key_file,ssl_key_password,verify_peer,verify_host,allow_traps,discover"	\
+		")"													\
+		" select"												\
+			" " ZBX_FS_UI64 ",type,snmp_oid,hostid,'%s','%s',delay,history,trends,status,value_type,"	\
+			"trapper_hosts,units,formula,logtimefmt,templateid,valuemapid,'%s',ipmi_sensor,authtype,"	\
+			"username,password,publickey,privatekey,flags,interfaceid,'%s',inventory_link,lifetime,"	\
+			"evaltype,jmx_endpoint,master_itemid,timeout,url,query_fields,posts,status_codes,"		\
+			"follow_redirects,post_type,http_proxy,headers,retrieve_mode,request_method,output_format,"	\
+			"ssl_cert_file,ssl_key_file,ssl_key_password,verify_peer,verify_host,allow_traps,discover"	\
+		" from items where itemid=" ZBX_FS_UI64
+
+	DB_EXEC(SQL, rdds80_templateid, "DDS80 enabled/disabled", "rdds80.enabled", "{$RSM.TLD.RDDS80.ENABLED}",
+		"History of RDDS80 being enabled or disabled.", rdds43_templateid);
+
+#undef SQL
+
+	/* update rdds43.enabled items */
+
+#define SQL	"update"												\
+			" items"											\
+		" set"													\
+			" name='RDDS43 enabled/disabled',"								\
+			" key_='rdds43.enabled',"									\
+			" params='{$RSM.TLD.RDDS43.ENABLED}',"								\
+			" description='History of RDDS43 being enabled or disabled.'"					\
+		" where"												\
+			" templateid=" ZBX_FS_UI64
+
+	DB_EXEC(SQL, rdds43_templateid);
+
+#undef SQL
+
+	/* create rdds80.enabled items */
+
+	result = DBselect("select itemid from items where templateid=" ZBX_FS_UI64, rdds43_templateid);
+
+	if (NULL == result)
+		goto out;
+
+#define SQL_ITEMS													\
+		"insert into items ("											\
+			"itemid,type,snmp_oid,hostid,name,key_,delay,history,trends,status,value_type,"			\
+			"trapper_hosts,units,formula,logtimefmt,templateid,valuemapid,params,ipmi_sensor,authtype,"	\
+			"username,password,publickey,privatekey,flags,interfaceid,description,inventory_link,lifetime,"	\
+			"evaltype,jmx_endpoint,master_itemid,timeout,url,query_fields,posts,status_codes,"		\
+			"follow_redirects,post_type,http_proxy,headers,retrieve_mode,request_method,output_format,"	\
+			"ssl_cert_file,ssl_key_file,ssl_key_password,verify_peer,verify_host,allow_traps,discover"	\
+		")"													\
+		" select"												\
+			" " ZBX_FS_UI64 ",type,snmp_oid,hostid,'%s','%s',delay,history,trends,status,value_type,"	\
+			"trapper_hosts,units,formula,logtimefmt," ZBX_FS_UI64 ",valuemapid,'%s',ipmi_sensor,authtype,"	\
+			"username,password,publickey,privatekey,flags,interfaceid,'%s',inventory_link,lifetime,"	\
+			"evaltype,jmx_endpoint,master_itemid,timeout,url,query_fields,posts,status_codes,"		\
+			"follow_redirects,post_type,http_proxy,headers,retrieve_mode,request_method,output_format,"	\
+			"ssl_cert_file,ssl_key_file,ssl_key_password,verify_peer,verify_host,allow_traps,discover"	\
+		" from items where itemid=" ZBX_FS_UI64
+
+#define SQL_RTDATA													\
+		"insert into item_rtdata (itemid,lastlogsize,state,mtime,error)"					\
+		"select " ZBX_FS_UI64 ",lastlogsize,state,mtime,error from item_rtdata where itemid=" ZBX_FS_UI64
+
+#define SQL_HISTORY													\
+		"insert into history_uint (itemid,clock,value,ns)"							\
+		"select " ZBX_FS_UI64 ",clock,value,ns from history_uint where itemid=" ZBX_FS_UI64
+
+	while (NULL != (row = DBfetch(result)))
+	{
+		zbx_uint64_t	rdds43_itemid;
+		zbx_uint64_t	rdds80_itemid;
+
+		ZBX_STR2UINT64(rdds43_itemid, row[0]);
+		rdds80_itemid = DBget_maxid_num("items", 1);
+
+		/* copy item */
+		DB_EXEC(SQL_ITEMS,
+			rdds80_itemid,
+			"RDDS80 enabled/disabled",
+			"rdds80.enabled",
+			rdds80_templateid,
+			"{$RSM.TLD.RDDS80.ENABLED}",
+			"History of RDDS80 being enabled or disabled.",
+			rdds43_itemid);
+
+		/* copy rtdata */
+		DB_EXEC(SQL_RTDATA, rdds80_itemid, rdds43_itemid);
+
+		/* copy history */
+		DB_EXEC(SQL_HISTORY, rdds80_itemid, rdds43_itemid);
+	}
+
+#undef SQL_ITEMS
+#undef SQL_RTDATA
+#undef SQL_HISTORY
+
+	ret = SUCCEED;
+out:
+	DBfree_result(result);
+
+	return ret;
+}
+
 #endif
 
 DBPATCH_START(5000)
@@ -232,5 +489,8 @@ DBPATCH_RSM(5000002, 1, 0, 1)	/* RSM FY21 */
 DBPATCH_RSM(5000002, 2, 0, 0)	/* move {$RSM.DNS.AVAIL.MINNS} from globalmacro to hostmacro, rename to {$RSM.TLD.DNS.AVAIL.MINNS} */
 DBPATCH_RSM(5000002, 3, 0, 0)	/* delete "rsm.configvalue[RSM.DNS.AVAIL.MINNS]" item */
 DBPATCH_RSM(5000002, 4, 0, 0)	/* replace "{$RSM.DNS.AVAIL.MINNS}" to "{$RSM.TLD.DNS.AVAIL.MINNS}" in item keys (template and hosts) */
+DBPATCH_RSM(5000002, 5, 0, 0)	/* split {$RSM.TLD.RDDS.ENABLED} macro into {$RSM.TLD.RDDS43.ENABLED} and {$RSM.TLD.RDDS80.ENABLED} */
+DBPATCH_RSM(5000002, 6, 0, 0)	/* replace {$RSM.TLD.RDDS.ENABLED} macro with {$RSM.TLD.RDDS43.ENABLED} and {$RSM.TLD.RDDS80.ENABLED} in rsm.dns[] and rsm.rdds[] item keys */
+DBPATCH_RSM(5000002, 7, 0, 0)	/* split rdds.enabled item into rdds43.enabled and rdds80.enabled */
 
 DBPATCH_END()
