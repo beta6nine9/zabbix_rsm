bc10204015ba757fdfa0074c57ce4541aee190b8
Andrejs Sitals <andrejs.sitals@zabbix.com>
2021-10-15 14:41:32 +0000

    .......... [RSM-1] added 405 as a supported status code in Provisioning API Forwarder

--- api/index.php
+++ api/index.php
@@ -155,6 +155,7 @@ function setCommonResponse(int $resultCode, string $title, ?string $description,
 		401, // Unauthorized
 		403, // Forbidden
 		404, // Not Found
+		405, // Method Not Allowed
 		500, // Internal Server Error
 	];
 
