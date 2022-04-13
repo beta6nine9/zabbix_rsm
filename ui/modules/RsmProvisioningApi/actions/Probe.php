6279e414846468eb325819b33522a616819d71b5
Andrejs Sitals <andrejs.sitals@zabbix.com>
2022-04-13 23:17:21 +0300

    ....I..... [RSM-1] 871: Provisioning API: added max length for probe's proxyPsk field

--- ui/modules/RsmProvisioningApi/actions/Probe.php
+++ ui/modules/RsmProvisioningApi/actions/Probe.php
@@ -48,7 +48,7 @@ class Probe extends ActionBaseEx
 							'proxyIp'           => ['type' => API_RSM_CUSTOM , 'function' => 'RsmValidateIP', 'error' => 'Invalid IP provided in the "proxyIp" element'],
 							'proxyPort'         => ['type' => API_RSM_CUSTOM , 'function' => 'RsmValidateInt', 'min' => 1, 'max' => 65535, 'error' => 'The "proxyPort" element must be a positive integer'],
 							'proxyPskIdentity'  => ['type' => API_STRING_UTF8, 'flags' => API_NOT_EMPTY, 'length' => 128],
-							'proxyPsk'          => ['type' => API_PSK        , 'flags' => API_NOT_EMPTY],
+							'proxyPsk'          => ['type' => API_PSK        , 'flags' => API_NOT_EMPTY, 'length' => 128],
 						]],
 						'online'                => ['type' => API_RSM_CUSTOM , 'function' => 'RsmValidateInvalid', 'error' => ' The "online" element was in the payload'],
 					]
