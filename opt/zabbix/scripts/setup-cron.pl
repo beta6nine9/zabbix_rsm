48de122ff93c99c7f2cfe41f65380e25adde6ce7
Andrejs Sitals <andrejs.sitals@zabbix.com>
2022-05-18 16:28:24 +0300

    ....I..... [RSM-1] 887: cron: run <service>.rollweek and <service>.downtime in parallel

--- opt/zabbix/scripts/setup-cron.pl
+++ opt/zabbix/scripts/setup-cron.pl
@@ -16,36 +16,55 @@ use constant LOG_DIR      => '/var/log/zabbix';
 use constant SCRIPTS_DIR  => '/opt/zabbix/scripts';
 use constant SLV_LOG_FILE => LOG_DIR . '/rsm.slv.err';
 
-use constant PID_FILE     => '/tmp/zabbix-rsm.probe.online.pl.cron.pid';
+use constant PROBE_PID_FILE => '/tmp/zabbix-rsm.probe.online.pl.cron.pid';
+use constant DNS_PID_FILE   => '/tmp/zabbix-rsm.slv.dns.avail.pl.cron.pid';
+use constant RDDS_PID_FILE  => '/tmp/zabbix-rsm.slv.rdds.avail.pl.cron.pid';
+use constant RDAP_PID_FILE  => '/tmp/zabbix-rsm.slv.rdap.avail.pl.cron.pid';
 
-my $JOB_USER = 'zabbix';	# default user of the job
+my $job_user = 'zabbix';	# default user of the job
 
-my $echo_pid = 'echo -n $$ > ' . PID_FILE . '; ';
-my $wait_pid = 'sleep 3; timeout 30 tail --pid=$(cat ' . PID_FILE . ') -f /dev/null; ';
+my $echo_probe_pid = 'echo -n $$ > ' . PROBE_PID_FILE . '; ';
+my $echo_dns_pid   = 'echo -n $$ > ' . DNS_PID_FILE   . '; ';
+my $echo_rdds_pid  = 'echo -n $$ > ' . RDDS_PID_FILE  . '; ';
+my $echo_rdap_pid  = 'echo -n $$ > ' . RDAP_PID_FILE  . '; ';
+
+my $wait_probe_pid = 'sleep 3; timeout 30 tail --pid=$(cat ' . PROBE_PID_FILE . ') -f /dev/null; ';
+my $wait_dns_pid   = 'sleep 3; timeout 45 tail --pid=$(cat ' . DNS_PID_FILE   . ') -f /dev/null; ';
+my $wait_rdds_pid  = 'sleep 3; timeout 45 tail --pid=$(cat ' . RDDS_PID_FILE  . ') -f /dev/null; ';
+my $wait_rdap_pid  = 'sleep 3; timeout 45 tail --pid=$(cat ' . RDAP_PID_FILE  . ') -f /dev/null; ';
 
 my @cron_jobs = (
 	'# ignore missing home directory errors',
 	'HOME=/tmp',
 	'',
-	['main', '* * * * *' , 0, $echo_pid . SCRIPTS_DIR . '/slv/rsm.probe.online.pl'                                                                                                                    , SLV_LOG_FILE],
+	['main', '* * * * *' , 0, $echo_probe_pid . SCRIPTS_DIR . '/slv/rsm.probe.online.pl'                                                                  , SLV_LOG_FILE],
 	'',
-	['main', '* * * * *' , 0, $wait_pid . '(' . SCRIPTS_DIR . '/slv/rsm.slv.dns.avail.pl && '    . SCRIPTS_DIR . '/slv/rsm.slv.dns.rollweek.pl && ' . SCRIPTS_DIR . '/slv/rsm.slv.dns.downtime.pl)'   , SLV_LOG_FILE],
-	['main', '* * * * *' , 0, $wait_pid . '(' . SCRIPTS_DIR . '/slv/rsm.slv.rdds.avail.pl && '   . SCRIPTS_DIR . '/slv/rsm.slv.rdds.rollweek.pl && ' . SCRIPTS_DIR . '/slv/rsm.slv.rdds.downtime.pl)' , SLV_LOG_FILE],
-	['main', '* * * * *' , 0, $wait_pid . '(' . SCRIPTS_DIR . '/slv/rsm.slv.rdap.avail.pl && '   . SCRIPTS_DIR . '/slv/rsm.slv.rdap.rollweek.pl && ' . SCRIPTS_DIR . '/slv/rsm.slv.rdap.downtime.pl)' , SLV_LOG_FILE],
-	['main', '* * * * *' , 0, $wait_pid . '(' . SCRIPTS_DIR . '/slv/rsm.slv.dnssec.avail.pl && ' . SCRIPTS_DIR . '/slv/rsm.slv.dnssec.rollweek.pl)'                                                   , SLV_LOG_FILE],
-	['main', '* * * * *' , 0, $wait_pid . '(' . SCRIPTS_DIR . '/slv/rsm.slv.dns.ns.avail.pl && ' . SCRIPTS_DIR . '/slv/rsm.slv.dns.ns.downtime.pl)'                                                   , SLV_LOG_FILE],
+	['main', '* * * * *' , 0, $echo_dns_pid  . $wait_probe_pid . SCRIPTS_DIR . '/slv/rsm.slv.dns.avail.pl'                                                , SLV_LOG_FILE],
+	['main', '* * * * *' , 0, $echo_rdds_pid . $wait_probe_pid . SCRIPTS_DIR . '/slv/rsm.slv.rdds.avail.pl'                                               , SLV_LOG_FILE],
+	['main', '* * * * *' , 0, $echo_rdap_pid . $wait_probe_pid . SCRIPTS_DIR . '/slv/rsm.slv.rdap.avail.pl'                                               , SLV_LOG_FILE],
 	'',
-	['main', '* * * * *' , 0, SCRIPTS_DIR . '/slv/rsm.slv.dns.udp.rtt.pl'                                                                                                                             , SLV_LOG_FILE],
-	['main', '* * * * *' , 0, SCRIPTS_DIR . '/slv/rsm.slv.dns.tcp.rtt.pl'                                                                                                                             , SLV_LOG_FILE],
-	['main', '* * * * *' , 0, SCRIPTS_DIR . '/slv/rsm.slv.rdds.rtt.pl'                                                                                                                                , SLV_LOG_FILE],
-	['main', '* * * * *' , 0, SCRIPTS_DIR . '/slv/rsm.slv.rdap.rtt.pl'                                                                                                                                , SLV_LOG_FILE],
+	['main', '* * * * *' , 0, $wait_dns_pid  . SCRIPTS_DIR . '/slv/rsm.slv.dns.rollweek.pl'                                                               , SLV_LOG_FILE],
+	['main', '* * * * *' , 0, $wait_rdds_pid . SCRIPTS_DIR . '/slv/rsm.slv.rdds.rollweek.pl'                                                              , SLV_LOG_FILE],
+	['main', '* * * * *' , 0, $wait_rdap_pid . SCRIPTS_DIR . '/slv/rsm.slv.rdap.rollweek.pl'                                                              , SLV_LOG_FILE],
 	'',
-	['main', '0 1 1 * *' , 0, SCRIPTS_DIR . '/disable-rdds-for-rdap-hosts.pl'                                                                                                                         , LOG_DIR . '/disable-rdds-for-rdap-hosts.err'],
-	['main', '0 15 1 * *', 0, SCRIPTS_DIR . '/sla-monthly-status.pl'                                                                                                                                  , LOG_DIR . '/sla-monthly-status.err'],
-	['main', '0 15 1 * *', 0, SCRIPTS_DIR . '/sla-report.php'                                                                                                                                         , LOG_DIR . '/sla-report.err'],
+	['main', '* * * * *' , 0, $wait_dns_pid  . SCRIPTS_DIR . '/slv/rsm.slv.dns.downtime.pl'                                                               , SLV_LOG_FILE],
+	['main', '* * * * *' , 0, $wait_rdds_pid . SCRIPTS_DIR . '/slv/rsm.slv.rdds.downtime.pl'                                                              , SLV_LOG_FILE],
+	['main', '* * * * *' , 0, $wait_rdap_pid . SCRIPTS_DIR . '/slv/rsm.slv.rdap.downtime.pl'                                                              , SLV_LOG_FILE],
 	'',
-	['main', '* * * * *' , 0, 'sleep 15; ' . SCRIPTS_DIR . '/config-cache-reload.pl'                                                                                                                  , LOG_DIR . '/config-cache-reload.log'],
-	['main', '* * * * *' , 0, 'sleep 45; ' . SCRIPTS_DIR . '/config-cache-reload.pl'                                                                                                                  , LOG_DIR . '/config-cache-reload.log'],
+	['main', '* * * * *' , 0, $wait_probe_pid . '(' . SCRIPTS_DIR . '/slv/rsm.slv.dnssec.avail.pl && ' . SCRIPTS_DIR . '/slv/rsm.slv.dnssec.rollweek.pl)' , SLV_LOG_FILE],
+	['main', '* * * * *' , 0, $wait_probe_pid . '(' . SCRIPTS_DIR . '/slv/rsm.slv.dns.ns.avail.pl && ' . SCRIPTS_DIR . '/slv/rsm.slv.dns.ns.downtime.pl)' , SLV_LOG_FILE],
+	'',
+	['main', '* * * * *' , 0, SCRIPTS_DIR . '/slv/rsm.slv.dns.udp.rtt.pl'                                                                                 , SLV_LOG_FILE],
+	['main', '* * * * *' , 0, SCRIPTS_DIR . '/slv/rsm.slv.dns.tcp.rtt.pl'                                                                                 , SLV_LOG_FILE],
+	['main', '* * * * *' , 0, SCRIPTS_DIR . '/slv/rsm.slv.rdds.rtt.pl'                                                                                    , SLV_LOG_FILE],
+	['main', '* * * * *' , 0, SCRIPTS_DIR . '/slv/rsm.slv.rdap.rtt.pl'                                                                                    , SLV_LOG_FILE],
+	'',
+	['main', '0 1 1 * *' , 0, SCRIPTS_DIR . '/disable-rdds-for-rdap-hosts.pl'                                                                             , LOG_DIR . '/disable-rdds-for-rdap-hosts.err'],
+	['main', '0 15 1 * *', 0, SCRIPTS_DIR . '/sla-monthly-status.pl'                                                                                      , LOG_DIR . '/sla-monthly-status.err'],
+	['main', '0 15 1 * *', 0, SCRIPTS_DIR . '/sla-report.php'                                                                                             , LOG_DIR . '/sla-report.err'],
+	'',
+	['main', '* * * * *' , 0, 'sleep 15; ' . SCRIPTS_DIR . '/config-cache-reload.pl'                                                                      , LOG_DIR . '/config-cache-reload.log'],
+	['main', '* * * * *' , 0, 'sleep 45; ' . SCRIPTS_DIR . '/config-cache-reload.pl'                                                                      , LOG_DIR . '/config-cache-reload.log'],
 );
 
 sub main()
@@ -55,7 +74,7 @@ sub main()
 
 	if (opt('user'))
 	{
-		$JOB_USER = getopt('user');
+		$job_user = getopt('user');
 	}
 	if (opt('enable'))
 	{
@@ -112,7 +131,7 @@ sub create_all($)
 			);
 
 			$crontab .= sprintf(
-				"%s%-${timing_len}s $JOB_USER %s%-${script_len}s >> %s 2>&1\n",
+				"%s%-${timing_len}s ${job_user} %s%-${script_len}s >> %s 2>&1\n",
 				$group_status->{$group}, $timing, $sleep, $script, $logfile
 			);
 		}
