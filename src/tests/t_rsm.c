cbf6502956bf926a08c5cdb7603bf9df4f3f1f52
dimir <vladimir.levijev@zabbix.com>
2022-07-07 18:03:02 +0300

    ....I..... [RSM-1] test utilities: always close the file handle before exiting the program

--- src/tests/t_rsm.c
+++ src/tests/t_rsm.c
@@ -8,28 +8,32 @@
 int	write_json_status(const char *json_file, const char *buffer, char **error)
 {
 	FILE	*f;
+	int	ret = FAIL;
 
 	if (NULL == (f = fopen(json_file, "w")))	/* w for write */
 	{
 		*error = zbx_dsprintf(*error, "cannot open file \"%s\" for writing: %s", json_file, strerror(errno));
-		return FAIL;
+		goto out;
 	}
 
 	if (1 > fwrite(buffer, 1, strlen(buffer), f))
 	{
 		*error = zbx_dsprintf(*error, "cannot write resulting JSON to file \"%s\"", json_file);
-		return FAIL;
+		goto out;
 	}
 
 	if (1 > fwrite("\n", sizeof(char), 1, f))
 	{
 		*error = zbx_dsprintf(*error, "cannot write resulting JSON to file \"%s\"", json_file);
-		return FAIL;
+		goto out;
 	}
 
-	fclose(f);
+	ret = SUCCEED;
+out:
+	if (f != NULL)
+		fclose(f);
 
-	return SUCCEED;
+	return ret;
 }
 
 void	alarm_signal_handler(int sig)
